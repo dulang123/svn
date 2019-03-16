18642b4c2b0ca81284da193dd0e0aed24280e99b
dulang123 <1282032739@qq.com>
2019-03-16 16:18:49 +0800

    试试

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
-
         System.out.println("ss");
+        System.out.println("ssss");
         System.out.println("ssss");
     }
 }
